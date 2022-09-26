c352d929a9e34177bccef7e5cb6492773ddb54f6
zermaineeee <zermaine.lew00@gmail.com>
2022-09-26 22:57:01 +0800

    add router template

--- GroupProject/client/src/App.vue
+++ GroupProject/client/src/App.vue
@@ -1,20 +1,6 @@
 <template>
-  <HeaderBar />
-  <!-- <HeaderBar2 /> -->
+  <router-view />
 </template>
-
-<script>
-// import HeaderBar2 from './components/HeaderBar2.vue';
-import HeaderBar from './components/HeaderBar.vue';
-
-export default {
-  name: 'App',
-  components: {
-    // HeaderBar2,
-    HeaderBar
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,5 +9,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- GroupProject/client/src/main.js
+++ GroupProject/client/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Oruga from '@oruga-ui/oruga-next'
 import '@oruga-ui/oruga-next/dist/oruga-full.css'
+import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(router)
 
 app.use(Oruga)
 app.mount('#app')
