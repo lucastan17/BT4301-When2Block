bd869fe48b7d8d4541c2cb37d92fdfc4c09941a9
hengjeejoo <jeejoo2@gmail.com>
2022-09-27 14:25:06 +0800

    Added leaflet for declarative map functions on Search Page

---
+++ GroupProject/client/src/components/SearchOverlay.vue
@@ -0,0 +1,151 @@
+<template>
+    <div id="navbar">
+        <div id="logo">
+            <img alt="When2Block Logo" src="../assets/when2block_logo.png" />
+        </div>
+        <div id="navlinks">
+            <ul>
+                <li>
+                    <a href="/">Check-In</a>
+                </li>
+                <li>
+                    <a href="/">Statistics</a>
+                </li>
+                <li>
+                    <a href="/">Search</a>
+                </li>
+                <li>
+                    <a href="/">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+
+        <div id="LogOut">
+            <p>Hi, <a href="/">USERNAME</a></p>
+
+            <o-button>
+                Log Out
+            </o-button>
+        </div>
+    </div>
+    <l-map style="height: 350px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng" title="Marker 1"></l-marker>
+  </l-map>
+</template>
+
+<script>
+import {
+  LMap,
+  //LIcon,
+  LTileLayer,
+  LMarker,
+  /*LControlLayers,
+  LTooltip,
+  LPopup,
+  LPolyline,
+  LPolygon,
+  LRectangle,*/
+} from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default {
+    name: 'HeaderBar',
+    props: {
+        msg: String
+    },
+    components: {
+    LMap,
+    //LIcon,
+    LTileLayer,
+    LMarker,
+    //LControlLayers,
+    //LTooltip,
+    //LPopup,
+    //LPolyline,
+    //LPolygon,
+    //LRectangle,
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      zoom: 10,
+      center: [1.3521, 103.8198],
+      markerLatLng: [1.3521, 103.75]
+    };
+  }
+}
+
+</script>
+
+<style scoped>
+#logo {
+    position: absolute;
+    top: 5px;
+    left: 15px;
+}
+
+#navbar {
+    display: flex;
+    justify-content: center;
+    background-color: rgb(40, 41, 62);
+    align-items: center;
+}
+
+#navlinks {
+    color: white;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+#LogOut {
+    color: white;
+    margin-right: 20px;
+    position: absolute;
+    right: 10px;
+}
+
+button {
+    background-color: rgb(241, 99, 9);
+    color: white;
+    border-radius: 5px;
+    height: 25px;
+}
+
+p {
+    margin: 0px;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 70px;
+}
+
+a {
+    color: white;
+}
+
+#navlinks a {
+    color: white;
+    text-decoration: none;
+}
+
+#navlinks a:hover,
+a.active {
+    border-bottom: white solid 2px;
+}
+
+/* to underline pages  */
+/* #navlinks a:active {
+    border-bottom: white solid 2px;
+} */
+</style>
+    
