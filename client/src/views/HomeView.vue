a5f953edf04e2fb591f97a06cc31bba3107c6a6c
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-09-30 16:36:18 +0800

    Edit BoxDisplay.vue: create display box component to display statistics for UserBehaviour page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <HeaderBar2 />
+    <BoxDisplay title="Hello" actualNumber="300" percentage="60%"></BoxDisplay>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeaderBar2 from '@/components/HeaderBar2.vue';
+import BoxDisplay from '@/components/BoxDisplay.vue';
+
 
 export default {
   name: 'HomeView',
   components: {
-    HeaderBar2
-  }
+    HeaderBar2,
+    BoxDisplay
+}
 }
 </script>
