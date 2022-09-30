da176d8c879422880988c449cf502a236d774b14
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-09-30 17:23:01 +0800

    Add UserBehaviourChart.vue: template for developing statistics chart

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <HeaderBar2 />
     <BoxDisplay title="Hello" actualNumber="300" percentage="60%"></BoxDisplay>
+    <UserBehaviourChart></UserBehaviourChart>
   </div>
 </template>
 
@@ -9,13 +10,15 @@
 // @ is an alias to /src
 import HeaderBar2 from '@/components/HeaderBar2.vue';
 import BoxDisplay from '@/components/BoxDisplay.vue';
+import UserBehaviourChart from '@/components/UserBehaviourChart.vue';
 
 
 export default {
   name: 'HomeView',
   components: {
     HeaderBar2,
-    BoxDisplay
+    BoxDisplay,
+    UserBehaviourChart
 }
 }
 </script>
