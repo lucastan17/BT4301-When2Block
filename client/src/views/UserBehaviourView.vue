b3cbaab2ef9583faab667cac02d1ee383f08795f
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-10-02 09:28:50 +0800

    Create UserBehaviourView.vue: Blank page for User Behvaiour dashboard

---
+++ client/src/views/UserBehaviourView.vue
@@ -0,0 +1,21 @@
+<template>
+    <HeaderBar></HeaderBar>
+</template>
+
+
+<script>
+import HeaderBar from '../components/HeaderBar.vue';
+
+export default {
+    name: "UserBeavhiourPage",
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        HeaderBar,
+    },
+}
+
+</script>
