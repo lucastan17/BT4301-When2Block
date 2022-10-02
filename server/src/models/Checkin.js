693c3a0c517a6c07d7516841f104df20af39690a
lucastan17 <48749186+lucastan17@users.noreply.github.com>
2022-10-02 23:25:40 +0800

    Adding models for mysql tables
    Added models for mysql tables using Sequelize

---
+++ server/src/models/Checkin.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+    const Checkin = sequelize.define('Checkin', {
+      //attributes and data types etc
+      user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+      },
+      created_date: {
+        type: DataTypes.DATETIME,
+        defaultValue: DataTypes.NOW,
+        primaryKey: true
+      }
+    })}
