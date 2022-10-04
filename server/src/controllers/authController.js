4ae9762e1ee2a2b3d1ef20436b8d70265466420d
lucastan17 <48749186+lucastan17@users.noreply.github.com>
2022-10-04 19:04:03 +0800

    connection to db and testing with register
    Connecting to database and testing with Register feature

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -13,9 +13,14 @@ module.exports = {
   async register (req, res) {
     try {
       //logic to send to db
+      const user = await User.create(req.body)
+      res.send(user.toJSON())
       //need email, pw
     } catch (err) {
       //error handling
+      res.status(400).send({
+        error: 'This email account is already in use.'
+      })
     }
   },
   async login (req, res) {
--- server/src/policies/authControllerPolicy.js
+++ server/src/policies/authControllerPolicy.js
@@ -3,5 +3,34 @@ const Joi = require('joi')
 module.exports = {
   register (req, res, next) {
     // validation schema for registration
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9]{8,32}$')
+      )
+    }
+
+    const {error, value} = Joi.validate(req.body, schema)
+    
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'You must provide a valid email address'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: 'The password provided did not match the rules'
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information'
+          })
+      }
+    } else {
+      next()
+    }
   }
 }
