bbc462dd79f8377f6f0f9930e6653d79e857ab12
zermaineeee <zermaine.lew00@gmail.com>
2022-10-04 11:16:02 +0800

    Login page FE

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,33 +1,34 @@
 <template>
-    <div class="home">
-        <HeaderBar2 title="Login">
+    <div>
+        <HeaderBar2 title="Log In">
+            <o-button class="button" @click="register">Register</o-button>
         </HeaderBar2>
         <br />
-        <Register />
+        <LogIn />
     </div>
 </template>
-  
+    
 <script>
 import HeaderBar2 from '@/components/HeaderBar2.vue';
+import LogIn from '@/components/LogIn.vue';
 
 export default {
-    name: 'LoginView',
-    components: {
-        HeaderBar2,
-    },
+    components: { HeaderBar2, LogIn },
     methods: {
-
+        register() {
+            this.$router.push({ name: "register" })
+        }
     }
 }
 </script>
-  
+    
 <style scoped>
 .button {
     background-color: #F16308;
     margin: 15px;
     border-radius: 5px;
     position: absolute;
-    right: 10px;
+    right: 5px;
 }
 </style>
-  
+    
