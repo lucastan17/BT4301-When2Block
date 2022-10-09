c898a98ed0fcbe0526fb9a431f2bf7328b81b7df
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-10-09 21:51:20 +0800

    Formatting change by linter

--- server/src/policies/isAdmin.js
+++ server/src/policies/isAdmin.js
@@ -1,3 +1,3 @@
 module.exports = function (req, res, next) {
-  //if authenticated and user type is admin, next() else error handling
+  // if authenticated and user type is admin, next() else error handling
 }
