fd6013b3ffa0efa9b3e2b98ff8c8e419ffe27d0e
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-10-14 18:34:39 +0800

    Add wieghtsFileController.js: Logic for POST request with model weights file

---
+++ server/src/controllers/weightsFileController.js
@@ -0,0 +1,29 @@
+const db = require('../models')
+const sequelize = db.sequelize
+const { QueryTypes } = require('sequelize')
+const fs = require('fs')
+
+module.exports = {
+  async post (req, res) {
+    try {
+      const index = await sequelize.query('SELECT MAX(model_id) as id FROM model', { type: QueryTypes.SELECT })
+      const newId = index[0].id + 1// would've incremented by one after adding text columns
+
+      const folderName = process.cwd() + '\\src\\production_models\\model_' + (newId).toString()
+      if (!fs.existsSync(folderName)) {
+        fs.mkdirSync(folderName)
+      }
+
+      const myFile = req.files.file
+      myFile.mv(folderName + '\\' + myFile.name, function (err) {
+        if (err) {
+          console.log(err)
+          return res.status(500).send({ msg: 'Error occured' })
+        }
+      })
+    } catch (err) {
+      // res.send('ERROR' + err)
+      // error handling
+    }
+  }
+}
