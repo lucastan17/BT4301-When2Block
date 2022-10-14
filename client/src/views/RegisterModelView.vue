04a742446a4953b8935e3158ee9b597b26b218b6
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-10-14 18:38:40 +0800

    Add RegisterModelView.vue: Complete FE view for register model page with header

---
+++ client/src/views/RegisterModelView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <HeaderBar/>
+        <ModelRegister />
+    </div>
+</template>
+    
+<script>
+import HeaderBar from '@/components/HeaderBar.vue';
+import ModelRegister from '@/components/ModelRegister.vue';
+
+export default {
+    components: { HeaderBar, ModelRegister }
+}
+</script>
+    
+<style scoped>
+
+</style>
+    
