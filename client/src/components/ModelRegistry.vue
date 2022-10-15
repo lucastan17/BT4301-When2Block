41bbf1f7dbbe27b125aa7a11b0417a587385657c
Vinodjayakumar124 <vinodjayakumar124@gmail.com>
2022-10-15 14:23:21 +0800

    Create ModelRegistry.vue: Main dashboard component for model registry page

---
+++ client/src/components/ModelRegistry.vue
@@ -0,0 +1,115 @@
+<template>
+    <div id="outer-cover">
+        <div id="title-row">
+            <h1>Model Registry</h1>
+            <o-button class="button" id="register-button" @click="registerNewModel()">Register A New Model</o-button>
+        </div>
+        <table id="model_registry_table">
+            <!-- TABLE HEADERS -->
+            <thead>
+                <tr>
+                    <th v-for="header in headers" :key="header">
+                        {{header}}
+                    </th>
+                </tr>
+            </thead>
+            <!-- TABLE ROWS -->
+            <tbody>
+                <tr v-for="datum in data" :key="datum.model_id">
+                    <td>{{datum.modelName}}</td>
+                    <td>{{datum.deploymentStatus}}</td>
+                    <td>{{datum.modelDescription}}</td>
+                    <td>{{datum.modelVersion}}</td>
+                    <td>{{datum.editedTime}}</td>
+                    <td>{{datum.modelPerformance}}</td>
+                    <td>
+                        <o-button class="button" v-if="datum.deploymentStatus != 'Deployed'" @click="onUse(datum.model_id)">Use</o-button>
+                        <p v-if="datum.deploymentStatus == 'Deployed'">In-Use</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            headers: ["Name of Model", "Deployment Status", "Description", "Version Number", "Updated Date", "Performance Metric (AUC)", "Use?"],
+            // data: [{'model_id':4 ,'modelName': 'Model 4', 'deploymentStatus': 'Staged', "modelDescription": "40 Hidden Layers", "modelVersion": "V2.5", "editedTime": "29/10/22", "modelPerformance": "0.99"},
+            //        {'model_id':3 ,'modelName': 'Model 3', 'deploymentStatus': 'Deployed', "modelDescription": "20 Hidden Layers", "modelVersion": "V1.2", "editedTime": "20/09/22", "modelPerformance": "0.89"},
+            //        {'model_id':2 ,'modelName': 'Model 2', 'deploymentStatus': 'Decommissioned', "modelDescription": "10 Hidden Layers", "modelVersion": "V0.5", "editedTime": "29/07/22", "modelPerformance": "0.12"},
+            //        {'model_id':1 ,'modelName': 'Model 1', 'deploymentStatus': 'Decommissioned', "modelDescription": "5 Hidden Layers", "modelVersion": "V0.2", "editedTime": "04/06/22", "modelPerformance": "0.58"}] 
+        } 
+    },
+    props: {
+        data: Array
+    },
+    methods: {
+        onUse(id) {
+            console.log(id)
+            //Lucas can add the logic for the POST request to change the deployment status field of the `model` table in DB to 'deployed' here
+        },
+        registerNewModel() {
+            this.$router.push("/register-model")
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+#outer-cover {
+    width: 85%;
+    margin: auto;
+}
+
+#title-row {
+    margin-top: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+#register-button {
+    margin-top: 20px;
+}
+
+h1 {
+    text-align: left;
+}
+
+tbody tr:nth-child(odd) {
+  background-color: #f4aa7b;
+
+}
+
+tbody tr:nth-child(even) {
+  background-color: rgb(249, 205, 118);
+}
+
+table {
+  background-color: #F16308;
+  width: 100%;
+  margin: auto;
+}
+
+tr {
+    height: 60px;
+}
+
+table td th {
+    text-align: center;
+}
+
+.button {
+    background-color: #F16308;
+    border: 0;
+    color: white;
+    margin-top: 7px;
+}
+
+
+</style>
