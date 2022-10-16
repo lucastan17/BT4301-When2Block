041b1787c51c36e3f85768006eaf0b28eb61ca47
p-chow <63701285+p-chow@users.noreply.github.com>
2022-10-16 22:22:08 +0800

    Revert "Models"

--- server/src/controllers/searchController.js
+++ server/src/controllers/searchController.js
@@ -1,28 +1,14 @@
 const db = require('../models')
 const sequelize = db.sequelize
 const { QueryTypes, Op } = require('sequelize')
-const results = db.results
-const today = new Date()
-
-function formatDate (dt) {
-  const day = dt.getDate()
-  const month = dt.getMonth() + 1
-  const year = dt.getFullYear()
-  return `${year}-${month}-${(day > 9 ? '' : '0') + day}`
-}
-
-// function sgTimeConverter(date) {
-//   date.setTime(date.getTime() + 8 * 60 * 60 * 1000)
-//   return date
-// }
+const Results = db.results
 
 module.exports = {
   async index (req, res) {
     const result = {}
     try {
-      // check if there is any data today, store in todayData
-      const todayData = await sequelize.query(`SELECT * FROM results where date(time) = date '${formatDate(today)}'`, { type: QueryTypes.SELECT })
-      result.today = todayData
+      const all = await sequelize.query('SELECT * FROM results', { type: QueryTypes.SELECT })
+      result.all = all
       // logic to retrieve from db
       // need location and time
       const currDate = await sequelize.query('SELECT cast(results.time as date) date FROM results WHERE results.location ="test"', { type: QueryTypes.SELECT })
