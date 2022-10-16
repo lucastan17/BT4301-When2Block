672b49494285ca95bbc467681487f7ecfd44eb47
downmeanhigh <daominhhai@u.nus.edu>
2022-10-16 17:28:06 +0800

    change db config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,23 +1,14 @@
 // const path = require('path')
 
 module.exports = {
-  port: 8081,
+  port: process.env.PORT || 3036,
   db: {
-    /* database: process.env.DB_NAME || 'when2block',
+    database: process.env.DB_NAME || 'when2block',
     user: process.env.DB_USER || 'when2block',
     password: process.env.DB_PASS || 'bt4301grp2',
     options: {
       dialect: process.env.DIALECT || 'mysql',
-      host: process.env.HOST || 'localhost' */
-    // }
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'when2block',
-    dialect: 'mysql',
-    options: {
-      dialect: 'mysql',
-      host: 'localhost'
+      host: process.env.HOST || 'localhost'
     }
   },
   authentication: {
