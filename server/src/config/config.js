9aa95191618f9b6bb3720788dc841784d443767f
downmeanhigh <daominhhai@u.nus.edu>
2022-10-19 13:36:39 +0800

    edit config db

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,24 +1,24 @@
 // const path = require('path')
 
 module.exports = {
-  port: process.env.PORT || 8081,
+  port: process.env.PORT || 3036,
   db: {
-    /* database: process.env.DB_NAME || 'when2block',
-        user: process.env.DB_USER || 'when2block',
-        password: process.env.DB_PASS || 'bt4301grp2',
-        options: {
-          dialect: process.env.DIALECT || 'mysql',
-          host: process.env.HOST || 'localhost' */
-    // }
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'when2block',
-    dialect: 'mysql',
+    database: process.env.DB_NAME || 'when2block',
+    user: process.env.DB_USER || 'when2block',
+    password: process.env.DB_PASS || 'bt4301grp2',
     options: {
-      dialect: 'mysql',
-      host: 'localhost'
+      dialect: process.env.DIALECT || 'mysql',
+      host: process.env.HOST || 'localhost'
     }
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'password',
+    // database: 'when2block',
+    // dialect: 'mysql',
+    // options: {
+    //   dialect: 'mysql',
+    //   host: 'localhost'
+    // }
   },
   authentication: {
     jwtSecret: process.env.JWT_SECRET || 'secret'
